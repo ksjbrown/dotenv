b2b3409c001e965334609269cc2925adb0332afa
Keegan <keeganbrown@hotmail.com>
2024-12-10 17:01:21 +0100

    add nvim config

---
+++ settings/nvim/init.lua
@@ -0,0 +1,12 @@
+vim.opt.relativenumber = true  -- Enables relative line numbers
+vim.opt.number = true          -- Enables the current line number
+vim.cmd("syntax on")
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.showmatch = true
+vim.opt.signcolumn = "yes"
+vim.cmd("colorscheme tokyonight-storm")
